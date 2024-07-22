b7c7a3890b501d532df521bdb6de92fd0945f43e
R4wd0G <wpa.sys@hotmail.com>
2024-07-22 17:48:01 -0300

    adicionado contagem de blocos por validador + momento atual

--- getSignerMetrics.js
+++ getSignerMetrics.js
@@ -10,6 +10,20 @@ async function main() {
   let enode = [];
   let account = [];
 
+  function formatDate() {
+    const now = new Date();
+    const hour = now.getHours().toString().padStart(2, '0');
+    const minute = now.getMinutes().toString().padStart(2, '0');
+    const second = now.getSeconds().toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Janeiro é 0
+    const year = now.getFullYear().toString().slice(-2); // Os dois últimos dígitos
+
+    return `${hour}:${minute}:${second} ${day}/${month}/${year}`;
+  }
+
+  const timeDateStamp = formatDate();
+
   await storeIntoArrays(instituicao, enode, account);
   const accountToInstituicao = await mapAccountToInstituicao(account, instituicao);
   
@@ -23,12 +37,20 @@ async function main() {
   const rpcMethod = 'qbft_getSignerMetrics';
   const rpcData = await makeRpcCall_signerMetrics(validatorNode.host, validatorNode.port, rpcMethod);
 
+
+  // Determine the maximum length for alignment
+const maxLength = rpcData.reduce((max, val) => Math.max(max, (accountToInstituicao[val.address] || val.address).length), 0);
+
+
+
   // Filtrar a saída pela chave 'address' e substituir pelo nome da instituição
   if (rpcData && Array.isArray(rpcData)) {
-  const filteredData = rpcData.map(data => {
-    const address = data.address;
-    return accountToInstituicao[address] || address;
-  }).sort();  // Sort the data alphabetically
+    const filteredData = rpcData.map(data => {
+      const address = accountToInstituicao[data.address] || data.address;
+      const lastProposedBlockNumber = parseInt(data.lastProposedBlockNumber, 16); // Converting hex to decimal
+      return `${address.padEnd(maxLength + 5)}[ Last Block: ${lastProposedBlockNumber} ]`;
+   }).sort(); // Sort the data alphabetically
+   
 
   const asciiArt = `
              _    _____ _                       __  __      _        _          
@@ -46,7 +68,7 @@ async function main() {
   // Add the line count at the end
   const output = [
     asciiArt,
-    'Last 100 blocks...',
+    `Last 100 blocks...\t\tTime: ${timeDateStamp}`,
     '',
     validatorNode.nodeName,
     '=====================================================',
